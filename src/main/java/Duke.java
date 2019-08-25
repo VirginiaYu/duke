1236595ccd6a1b33c41979f1e12063f6f525008b
Yu Jiahan <116010274@link.cuhk.edu.cn>
2019-08-25 21:10:20 +0800

    Level-3: Mark as Done

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -2,36 +2,79 @@ import java.util.Scanner;
 
 public class Duke {
     public static void main(String[] args) {
+        // Greetings!
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello! I am\n" + logo);
-        System.out.println("What can I do for you?\n");
+        System.out.println("What can I do for you?");
 
         // Declare the object and initialize with
         // predefined standard input object
         Scanner sc = new Scanner(System.in);
-        // Create a new String array
-        String[] strLst = new String[100];
+
+        // Create a new task array
+        Task[] taskArray = new Task[100];
+
+        // Count items in array
         int cnt=0;
+
         while (true) {
+
             // String input
             String input = sc.nextLine();
+
+            // Exit
             if (input.equals("bye")) {
-                System.out.println("Bye. Hope to see you again soon!\n");
+                System.out.println("Bye. Hope to see you again soon!");
                 System.exit(0);
-            } else if (input.equals("list")) {
-                for(int i = 0; i < cnt; i++) {
-                    int number = i+1;
-                    System.out.println(number + ". " + strLst[i]);
+            }
+
+            // List tasks
+            else if (input.equals("list")) {
+                // If no task added
+                if (cnt==0)
+                    System.out.println("No task in your list!");
+                // List all task w/ their status
+                else {
+                    System.out.println("Here are the tasks in your list:");
+                    for(int i = 0; i < cnt; i++) {
+                        int number = i+1;
+                        System.out.println(number+".["+taskArray[i].getStatusIcon()+"] "+taskArray[i].getDescription());
+                    }
                 }
-            } else{
-                System.out.println("added: "+ input);
+            }
+
+            // Mark the task as done
+            else if (input.startsWith("done")) {
+                int index = Integer.parseInt(input.split(" ")[1]);
+                taskArray[index-1].markAsDone();
+            }
+
+            // Add item
+            else{
+                System.out.println("added: "+input);
                 // Add user input into string array
-                strLst[cnt++] = input;
+                taskArray[cnt++]= new Task(input);
             }
         }
     }
 }
+
+
+
+//list
+//    ____________________________________________________________
+//     Here are the tasks in your list:
+//     1.[✓] read book
+//     2.[✗] return book
+//     3.[✗] buy bread
+//    ____________________________________________________________
+//
+//done 2
+//    ____________________________________________________________
+//     Nice! I've marked this task as done:
+//       [✓] return book
+//
---
+++ src/main/java/Task.java
@@ -0,0 +1,28 @@
+public class Task {
+        protected String description;
+        protected boolean isDone;
+
+        public Task(String description) {
+            this.description = description;
+            this.isDone = false;
+        }
+
+        public String getStatusIcon() {
+            return (isDone ? "\u2713" : "\u2718"); //return tick or X symbols
+        }
+
+        public String getDescription() {
+            return this.description;
+        }
+
+//        public void setDescription(String s) {
+//            this.description = s;
+//        }
+
+        public void markAsDone() {
+            this.isDone = true;
+            System.out.println("Nice! I've marked this task as done:");
+            System.out.println("  [\u2713] "+this.description);
+        }
+}
+
